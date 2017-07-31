ce5ed016b7bffc5b567a9cea7c3513219885df0f
Scott Hoyt <scottrhoyt@gmail.com>
2017-07-31 15:52:51 -0700

    Add doc comments.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 var jwt = require('jsonwebtoken');
 
+/**
+ * Parses and validates a filename of the form: <base64-email>.<id>.<extension>
+ *
+ * If the filename cannot be parsed, decoded, or validated, the function throws
+ * an error.
+ *
+ * @param {string} keyID - The Music Key ID available from your apple developer account.
+ * @param {string} teamID - The Team ID associated with your apple developer account.
+ * @param {string|buffer} secret - The secret to sign the JWT with. Available from your apple developer account.
+ * @return {string} - The JWT signed and encoded into a string.
+ */
 module.exports.generate = function(keyID, teamID, secret, callback) {
   var options = {
     issuer: teamID,
